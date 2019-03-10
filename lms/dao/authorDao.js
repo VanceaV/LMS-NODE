580cda74026f492e6d583bdf22cf3011ea263986
Vladimir Vancea <vancea.vladimir@gmail.com>
2019-03-10 00:24:07 -0500

    first commit

---
+++ lms/dao/authorDao.js
@@ -0,0 +1,63 @@
+var db = require('./db');
+
+exports.getAllAuthors = function(cb){
+    db.query("select * from tbl_author", (err,result,fields)=> {
+        cb(err, result,fields);
+      });
+};
+
+
+
+exports.addAuthor = function(author, cb){
+  db.beginTransaction(function(err){
+      if(err) cb(err, null);
+  
+      db.query('insert into tbl_author (author_name) values (?)',author.authorName, function(err, res){
+        if(err){
+          db.rollback(function(err, res){
+            cb(err, res);
+          });
+        } 
+        db.commit(function(err, res){
+          cb(err, res);
+        });
+      });
+    });
+};
+
+exports.updateAuthor = function(author, cb){
+  db.beginTransaction(function(err){
+      if(err) cb(err, null);
+  
+      db.query('update tbl_author set author_name=? where author_id=?',[author.authorName,author.authorId], function(err, res){
+        if(err){
+          db.rollback(function(err, res){
+            cb(err, res);
+          });
+        } 
+        db.commit(function(err, res){
+          cb(err, res);
+        });
+      });
+    });
+};
+
+
+exports.removeAuthor = function(authorId, cb){
+  db.beginTransaction(function(err){
+      if(err) cb(err, null);
+  
+      db.query('delete from tbl_author where author_id = ?', [authorId], function(err, res){
+        if(err){
+          db.rollback(function(err, res){
+            cb(err, res);
+          });
+        } 
+        db.commit(function(err, res){
+          cb(err, res);
+        });
+      });
+    });
+  }
+
+
---
+++ lms/dao/bookDao.js
@@ -0,0 +1,61 @@
+var db = require('./db');
+
+exports.getAllBooks = function(cb){
+    db.query('select * from tbl_book', function(err, result) {
+        cb(err, result);
+      });
+};
+
+exports.addBook = function(book, cb){
+    db.beginTransaction(function(err){
+        if(err) cb(err, null);
+    
+        db.query('insert into tbl_book(title, auth_id, pub_id) values(?,?,?)', [book.title, book.author.id,book.publisher.id], function(err, res){
+          if(err){
+            db.rollback(function(err, res){
+              return cb(err, res);
+            });
+          } 
+          db.commit(function(err, res){
+            cb(err, res);
+          });
+        });
+      });
+};
+
+
+exports.updateBook = function(book, cb){
+  db.beginTransaction(function(err){
+      if(err) cb(err, null);
+  
+      db.query('update tbl_book set title=?, auth_id=?, pub_id=? where book_id=?', [book.title, book.author.id,book.publisher.id,book.bookId], function(err, res){
+        if(err){
+          db.rollback(function(err, res){
+            cb(err, res);
+          });
+        } 
+        db.commit(function(err, res){
+          cb(err, res);
+        });
+      });
+    });
+};
+
+
+
+exports.removeBook = function(bookId, cb){
+    db.beginTransaction(function(err){
+        if(err) cb(err, null);
+    
+        db.query('delete from tbl_book where book_id = ?', [bookId], function(err, res){
+          if(err){
+            db.rollback(function(err, res){
+              cb(err, res);
+            });
+          } 
+          db.commit(function(err, res){
+            cb(err, res);
+          });
+        });
+      });
+}
